77439b84f30497d5ffce9a5dd6c36cb784f8980c
Lux <ira.k.khan@outlook.com>
2026-02-23 03:45:24 -0700

    Added visualization with pygame

--- src/__main__.py
+++ src/__main__.py
@@ -1,11 +1,20 @@
 from .simulation import simulationController
+from .visualization import visualizer
 import logging
+import pygame 
 
 def main():
     simController = simulationController.SimulationController()
-
-    for _ in range(5000):
+    visualize = visualizer.Visualizer(simController)
+    clock = pygame.time.Clock()
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                return
         simController.tick()
+        visualize.render()
+        clock.tick(60)  # Limit to 60 FPS
 
 if __name__ == "__main__":
     main()
